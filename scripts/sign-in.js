fe012e4b730b65d04cf11049a095527322a5dd9f
kiril212387 <kiril212387@gmail.com>
2026-09-21 14:30:59 +0300

    handling form data

--- scripts/sign-in.js
+++ scripts/sign-in.js
@@ -4,6 +4,9 @@ const signInFormElement = formElements.namedItem("sign-in");
 
 const passwordInput = signInFormElement.elements.namedItem("password");
 const passwordVisibilityCheckbox = signInFormElement.elements.namedItem("password-visibility");
+const firstNameInput = signInFormElement.elements.namedItem("first-name");
+const lastNameInput = signInFormElement.elements.namedItem("last-name");
+const emailInput = signInFormElement.elements.namedItem("email");
 
 function showPassword() {
     passwordInput.type = "text";
@@ -18,6 +21,21 @@ if (passwordVisibilityCheckbox.checked) {
 } else {
     hidePassword();
 }
+
+signInFormElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const payload = {
+        first_name: firstNameInput.value,
+        last_name: lastNameInput.value,
+        email: emailInput.value,
+        password: sha256(passwordInput.value),
+    };
+
+    const json = JSON.stringify(payload);
+
+    console.alert(json);
+});
 
 passwordVisibilityCheckbox.addEventListener("change", () => {
     if (passwordVisibilityCheckbox.checked) {
